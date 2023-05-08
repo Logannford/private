a5eb55c82c1789d6ddc29dcfdd34e22ac994a8f1
Logan Ford <logan@hiyield.co.uk>
2023-05-08 19:29:27 +0100

    testing different auth methods for effiency

--- pages/login.vue
+++ pages/login.vue
@@ -1,38 +1,54 @@
 <template>
-<form class="" @submit.prevent="handleLogin">
-	<div class="">
-		<h1 class="">Supabase + Nuxt 3</h1>
-		<p class="">Sign in via magic link with your email below</p>
-		<div>
-			<input 
-				class="inputField" 
-				type="email" 
-				placeholder="Your email" 
-				v-model="email" 
-			/>
+	<form class="px-10" @submit.prevent="handleLogin">
+		<div 
+			class="
+				h-full w-full bg-gray-900 rounded-md 
+				bg-clip-padding backdrop-filter 
+				backdrop-blur-sm bg-opacity-40 border border-black
+				px-6 py-3 flex flex-col gap-y-4
+				"
+			>
+			<p class="text-white text-center font-bold text-xl font-thin">Log in via magic link with your email below</p>
+			<div>
+				<input 
+					class="bg-transparent border-b border-black px-4 py-2" 
+					type="email" 
+					placeholder="Your email" 
+					v-model="email" 
+				/>
+			</div>
+			<div v-if="emailError" class="">
+				error
+			</div>
+			<div class="w-full">
+				<input
+					type="submit"
+					class="border border-black rounded-lg px-4 py-2 w-full"
+					:value="loading ? 'Loading' : 'Send magic link'"
+					:disabled="loading"
+				/>
+			</div>
 		</div>
-		<div>
-			<input
-				type="submit"
-				class="button block"
-				:value="loading ? 'Loading' : 'Send magic link'"
-				:disabled="loading"
-			/>
-		</div>
-	</div>
 	</form>
 </template>
 
 <script setup>
-const supabase = useSupabaseClient()
+import { error } from 'console'
 
-const loading = ref(false)
-const email = ref('')
+const supabase = useSupabaseClient();
+
+const loading = ref(false);
+const email = ref('');
+const emailError = ref(false);
 
 const handleLogin = async () => {
+	email: email.value;
 	try {
+		if(!email)
+			emailError = true;
 		loading.value = true
-	const { error } = await supabase.auth.signInWithOtp({ email: email.value });
+	//sign the user in with a OTP which is sent to the email address entered in the input
+	const { error } = await supabase.auth.signInWithOtp({ email });
 
     if (error) 
 		throw error
