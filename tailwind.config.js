71ceee046e74ff81d377bd364df16bd5a6fee003
Logan Ford <logan@hiyield.co.uk>
2023-05-12 13:35:48 +0100

    some more tailwind config for vue transitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  css: [
+    `.v-enter-active,
+    .v-leave-active {
+      transition: opacity 0.5s ease;
+    }
+    
+    .v-enter-from,
+    .v-leave-to {
+      opacity: 0;
+    }`
+  ],
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
