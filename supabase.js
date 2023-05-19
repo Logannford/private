9c076409daa5b2d17966f89f7154c4d2fc1c6693
Logan Ford <logan@hiyield.co.uk>
2023-05-19 15:21:23 +0100

    supaaaa

--- supabase.js
+++ supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default (_, inject) => {
-	inject('supabase', supabase)
+	inject('supabase', supabase);
 }
