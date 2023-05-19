adbfd61768484746ac2c18d7b4819f096b4e5f6c
Logan Ford <logan@hiyield.co.uk>
2023-05-19 14:51:54 +0100

    supabase key

--- supabase.js
+++ supabase.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
-import { env } from "./"
+import { env } from 'process';
 
 const supabaseUrl = 'https://vbaptqkeeztnqztpdaos.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiYXB0cWtlZXp0bnF6dHBkYW9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM1Njc3MzksImV4cCI6MTk5OTE0MzczOX0.k8W8Br1DluExsAzOHdzErhm1bSwVlUjQjDw0nw695h4"
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default (_, inject) => {
+	inject('supabase', supabase)
+}
