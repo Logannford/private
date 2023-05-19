f25e40871374d52d717feaa56104d1f0b4f2366b
Logan Ford <logan@hiyield.co.uk>
2023-05-19 14:41:29 +0100

    plz work v2

--- supabase.js
+++ supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-import { env } from "./"
 
 const supabaseUrl = 'https://vbaptqkeeztnqztpdaos.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiYXB0cWtlZXp0bnF6dHBkYW9zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MzU2NzczOSwiZXhwIjoxOTk5MTQzNzM5fQ.w9yRZzpIfqQxDGRq9vWdZyq5qXgiEEB3tXzBdUzw-Qk"
+const supabaseKey = process.env.supabaseKey;
 const supabase = createClient(supabaseUrl, supabaseKey)
