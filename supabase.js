45aa20ceef70a493f49203488d7452551b18be2b
Logan Ford <logan@hiyield.co.uk>
2023-05-19 14:42:54 +0100

    supabase url

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://vbaptqkeeztnqztpdaos.supabase.co'
-const supabaseKey = process.env.supabaseKey
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
