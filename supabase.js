94dc896e9d6a500fef4950b8c7f92db9fbe73472
Logan Ford <logan@hiyield.co.uk>
2023-05-19 15:02:54 +0100

    maybe work?

--- supabase.js
+++ supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-import { env } from 'process';
 
 const supabaseUrl = 'https://vbaptqkeeztnqztpdaos.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiYXB0cWtlZXp0bnF6dHBkYW9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM1Njc3MzksImV4cCI6MTk5OTE0MzczOX0.k8W8Br1DluExsAzOHdzErhm1bSwVlUjQjDw0nw695h4"
+const supabaseKey = process.env.VITE_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default (_, inject) => {
