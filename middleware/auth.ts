e6027afed8e68ea0bacdea486dbd07b14492bb6d
Logan Ford <logan@hiyield.co.uk>
2023-05-20 15:53:27 +0100

    fixed small issue with infinite redirects

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,8 +1,11 @@
 export default defineNuxtRouteMiddleware((to) => {
 	const user = useSupabaseUser();
 
-	if(user.value)
-		navigateTo("/welcome");
-	else
-		navigateTo("/");	
+	if (!user.value) {
+		if (to.path !== '/welcome') {
+			navigateTo('/welcome');
+		}
+	} else if (to.path === '/welcome') {
+		navigateTo('/');
+	}
 })
--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,10 @@
 </template>
 
 <script setup>
+	//get a user object from supabase
 	const user = useSupabaseUser();
 
+	//on the page mounted, watch the user value
 	onMounted(() => {
 		watchEffect(() => {
 			if(!user.value)
