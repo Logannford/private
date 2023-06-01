23b1b4b7e60fd526a9d87ccde859a4c3cd928de5
Logan Ford <logan@hiyield.co.uk>
2023-06-01 10:29:20 +0100

    getting used to computed + pinia

--- pages/sign-up/onboarding.vue
+++ pages/sign-up/onboarding.vue
@@ -1,21 +1,31 @@
 <template>
 	<div>
-
+		<button @click="() => store.increment++">
+			click me
+		</button>
 	</div>
 </template>
 
 <script setup lang="ts">
 	//imports 
-	import { onMounted } from 'vue';
+	import { onMounted, watch } from 'vue';
 	import { onboardingProgress } from '~/store/index';
+	import { storeToRefs } from 'pinia'
 
+	//store
 	const store = onboardingProgress();
+	const { number } = storeToRefs(store);
+
 	//composables
 	//getting the active slide
 	const activeSlide = currentSlide();
 	
 	//lifecycle hooks
 	onMounted(() => {
-		console.log(activeSlide);
+		console.log(number.value);
+	})
+	//watchers
+	watch(number, (newNumber) => {
+		console.log(newNumber);
 	})
 </script>
--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,19 @@
 import { defineStore } from "pinia";
 
-export const onboardingProgress = defineStore({
-	id: "store",
+export const onboardingProgress = defineStore("onboarding-progress", () => {
 
+	//data properties
+	const number = ref<number>(2);
+
+	//getters
+	const increment = computed({
+		get: () => number.value,
+		set: (newValue: number) => {
+			number.value = newValue
+		}
+	})
+
+	//actions
+
+	return { number, increment }
 })
